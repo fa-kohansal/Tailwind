343ae97bf82e83c05e77c33cf98aa06425aa8627
fa-kohansal <farnaz_kohansal@yahoo.com>
2024-09-07 23:41:42 -0700

    create parallax and a part of the menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      bgImage:{
+        'parallax':"url('/src/img/parallax.jpg')"
+      }
+    },
   },
   plugins: [],
 }
